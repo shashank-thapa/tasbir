5602c1d07b8c4b76c91fa01baa4b20806f745827
Shashank Thapa <shashank.thapa.mgr@gmail.com>
2019-12-20 10:37:55 +0545

    v2

--- photo_app/models.py
+++ photo_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from user_app.models import UserModel
 # Create your models here.
+class PostModel(models.Model):
+    created = models.DateTimeField(auto_now_add = True)
+    location = models.CharField(max_length = 255)
+    uploaded_by = models.ForeignKey(UserModel, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='user_uploads')
+    likes = models.PositiveIntegerField(default=0)
+    liked_by = models.ManyToManyField(UserModel, related_name='liked_photos', blank=True)
+
+    def __str__(self):
+        return str(self.created)
+
+
+class CommentModel(models.Model):
+    created = models.DateTimeField(auto_now_add = True)
+    text = models.TextField()
+    commented_by = models.ForeignKey(UserModel, on_delete = models.CASCADE)
+    parent_post = models.ForeignKey(PostModel, on_delete = models.CASCADE)
--- user_app/urls.py
+++ user_app/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', ),
+
     
 ]
